b17695b562e0cb289d1f2138a274321f1e04218c
Silvio Souza <silviocortes@yahoo.com>
2019-05-11 21:13:20 -0500

    adding information about each project, from website

--- ReverseString/Program.cs
+++ ReverseString/Program.cs
@@ -2,6 +2,23 @@
 
 namespace ReverseString
 {
+
+    //      Write a function that reverses a string. The input string is given as an array of characters char[].
+
+    //      Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+    //      You may assume all the characters consist of printable ascii characters.
+
+ 
+
+    //      Example 1:
+
+    //      Input: ["h","e","l","l","o"]
+    //      Output: ["o","l","l","e","h"]
+    //      Example 2:
+
+    //      Input: ["H","a","n","n","a","h"]
+    //      Output: ["h","a","n","n","a","H"]
     class MainClass
     {
         public static void Main(string[] args)
@@ -16,8 +33,6 @@ namespace ReverseString
                 ReverseC(input);
                 Console.WriteLine(new string(input));
 
-                ReverseAlternative(input);
-                Console.WriteLine(new string(input));
 
             } while (!(Console.ReadLine().Equals("end")));
         }
@@ -40,6 +55,8 @@ namespace ReverseString
         public static void ReverseC(char[] s)
         {
             Array.Reverse(s);
+        }
+
         }
     }
 }
